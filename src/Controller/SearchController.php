db20d826593384878f6e7dafd70542f04ddd52e3
xclusive <xmt@abv.bg>
2026-01-12 09:00:58 +0200

    Fix language switcher

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route(path: ['bg' => '', 'en' => '/en'])]
 class SearchController extends AbstractController
 {
     /**
